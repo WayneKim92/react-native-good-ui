3e1145c7a533088f54a4f9f52bcd30fda7c2981c
eunchankim <eunchankim@kyobobook.com>
2023-01-04 10:16:18 +0900

    fix: remove code no longer needed

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,2 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
 export * from './components';
 export * from './utils';
-
-export function multiply(a: number, b: number): Promise<number> {
-  return Promise.resolve(a * b);
-}
-
-export function Hello() {
-  return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
-  );
-}
